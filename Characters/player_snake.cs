3b7fde090fc15e23761a314595744cd7d81a4250
Carl Blasius <carlblasius@gmail.com>
2023-03-24 15:00:42 +0100

    Action Mappings

--- Characters/player_snake.cs
+++ Characters/player_snake.cs
@@ -3,5 +3,15 @@ using System;
 
 public partial class player_snake : CharacterBody2D
 {
+    int speed = 1;
+    Vector2 InputDirection = new(0, 0);
 
+
+    public override void _PhysicsProcess(double delta)
+    {
+        base._PhysicsProcess(delta);
+        //Input.ActionPress("");
+        //Input.GetActionStrength("");
+        GD.Print(InputDirection);
+    }
 }
--- Levels/game_level.cs
+++ Levels/game_level.cs
@@ -1,7 +1,7 @@
 using Godot;
 using System;
 
-public partial class game_level : Node2D
+public partial class Game_level : Node2D
 {
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
