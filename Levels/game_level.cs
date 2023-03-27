f9b7122dc5bad34526631a458d83046e42e2bb0d
Carl Blasius <carlblasius@gmail.com>
2023-03-27 13:01:33 +0200

    Engine Update

--- Levels/game_level.cs
+++ Levels/game_level.cs
@@ -3,14 +3,14 @@ using System;
 
 public partial class Game_level : Node2D
 {
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        GD.Print("Start Level");
-    }
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		GD.Print("Start Level");
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-    {
-    }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
+	{
+	}
 }
