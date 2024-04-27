86d03269a077342f1ef69a660fdd3a4e21527cb8
Carl Blasius <carlblasius@gmail.com>
2024-04-27 19:09:36 +0200

    added rider and c++ scripts

---
+++ Source/Seagulls/CustomMapGenerator.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CustomMapGenerator.h"
+
+CustomMapGenerator::CustomMapGenerator()
+{
+}
+
+CustomMapGenerator::~CustomMapGenerator()
+{
+}
